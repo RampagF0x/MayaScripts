92fb11c71cd7e771dcd06a616495f04f05deee89
Administrator <854650422@qq.com>
2018-01-08 17:58:33 +0800

    180108

--- ProxyWindow.py
+++ ProxyWindow.py
@@ -1,17 +1,20 @@
 # coding=utf-8
 import maya.cmds as cmds
-def checkRender(any):
-    renderer = cmds.getAttr("defaultRenderGlobals.ren")
+
+renderer = cmds.getAttr("defaultRenderGlobals.ren")
+
 def ProxyWindow(any):
     if cmds.window('Proxy_optionsWindow', ex=True):
         cmds.deleteUI('Proxy_optionsWindow', wnd=True)
     win2 = cmds.window('Proxy_optionsWindow', title='Proxy_Window', widthHeight=(300, 200))
     cmds.columnLayout(adj=True)
-    cmds.button(en=False, l='')
-    cmds.button(l=u'Arnod代理')
-    cmds.textFieldGrp( label='Group 1', text='Editable')
+    cmds.button(l=renderer+u'代理',en=False)
+    if renderer is not "Arnold" or "redshift" or "Vray":
+        cmds.button(l=u"不支持当前渲染器代理", en=False)
+    else:
+        cmds.textFieldGrp( label='Group 1', text='Editable')
     cmds.columnLayout(adj=True)
-    cmds.button(l=u'Redshift代理')
+    #cmds.button(l=u'Redshift代理')
     cmds.showWindow(win2)
 
 ProxyWindow(any)
