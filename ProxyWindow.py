4f9129adc7f3fb83d76d2a5284163629feff4fa0
RampageFox <854650422@qq.com>
2018-01-08 13:47:23 +0800

    module

---
+++ ProxyWindow.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+import maya.cmds as cmds
+def ProxyWindow(any):
+    if cmds.window('Proxy_optionsWindow', ex=True):
+        cmds.deleteUI('Proxy_optionsWindow', wnd=True)
+    win2 = cmds.window('Proxy_optionsWindow', title='Proxy_Window', widthHeight=(300, 200))
+    cmds.columnLayout(adj=True)
+    cmds.button(en=False, l='')
+    cmds.button(l='Arnold代理')
+    cmds.columnLayout(adj=True)
+    cmds.button(l='Redshift代理')
+    cmds.showWindow(win2)
